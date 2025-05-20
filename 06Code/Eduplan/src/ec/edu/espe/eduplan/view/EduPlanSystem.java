186b0354ec5f769c958b694d017f268864c1e8d9
David Alejandro Bonilla Caiza <davidbonilla188@gmail.com>
2025-05-20 16:23:08 -0500

    Implementing the full login interface

--- 06Code/Eduplan/src/ec/edu/espe/eduplan/view/EduPlanSystem.java
+++ 06Code/Eduplan/src/ec/edu/espe/eduplan/view/EduPlanSystem.java
@@ -9,31 +9,50 @@ import ec.edu.espe.eduplan.model.User;
 
 public class EduPlanSystem {
     public static void main(String[] args) {
-        
-        //Creating the console interface
-        Menu menu =new Menu();
-        
-        int option = menu.showUserMenu();
-        
-         switch (option) {
-            case 1:
-                User user = menu.showLoginMenu();
-                
-                
-                
-                
-                
-                
-                break;
-            case 2:
-                menu.showRegistrationMenu();
-                break;
-            case 3:
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Opción no válida.");
+        Menu menu = new Menu();
+        boolean exit = false;
+
+        while (!exit) {
+            int option = menu.showUserMenu();
+
+            switch (option) {
+                case 1:
+                    User userLogin = menu.showLoginMenu();
+                    if (userLogin != null) {
+                        switch (userLogin.getRol()) {
+                            case "Director":
+                                // Aquí va el menú para director
+                                break;
+                            case "Maestro":
+                                // Aquí va el menú para maestro
+                                break;
+                        }
+                    }
+                    break;
+
+                case 2:
+                    User userRegister = menu.showRegistrationMenu();
+                    if (userRegister != null) {
+                        switch (userRegister.getRol()) {
+                            case "Director":
+                                // Aquí va el menú para director
+                                break;
+                            case "Maestro":
+                                // Aquí va el menú para maestro
+                                break;
+                        }
+                    }
+                    break;
+
+                case 3:
+                    exit = true;
+                    System.out.println("Saliendo del sistema...");
+                    break;
+
+                default:
+                    System.out.println("Opción no válida.");
+            }
         }
-        
     }
 }
+
